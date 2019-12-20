e1a61a8813981f202da018710a7f5d9876b03015
Cristian Torres <c.torres@mdxsolutions.co.uk>
2019-12-20 11:42:40 +0000

    Score added

--- lib/main.dart
+++ lib/main.dart
@@ -13,19 +13,42 @@ class _MyAppState extends State<MyApp> {
   final List<Map<String, dynamic>> _questions = const [
     {
       'questionText': 'What\'s your favorite color?',
-      'answers': ['Black', 'Red', 'White']
+      'answers': [
+        {'text': 'Black', 'score': 10},
+        {'text': 'Red', 'score': 5},
+        {'text': 'White', 'score': 3},
+        {'text': 'Green', 'score': 1},
+      ]
     },
     {
       'questionText': 'What\'s your favorite animal?',
-      'answers': ['Elephant', 'Cat', 'Spider', 'Rabbit']
+      'answers': [
+        {'text': 'Elephant', 'score': 10},
+        {'text': 'Cat', 'score': 8},
+        {'text': 'Snake', 'score': 7},
+        {'text': 'Lion', 'score': 5},
+      ]
     },
     {
       'questionText': 'Who is your favourite instructor',
-      'answers': ['Max', 'Max', 'Max', 'Max']
+      'answers': [
+        {'text': 'Cristian', 'score': 50},
+        {'text': 'Fernando Herrera', 'score': 25},
+        {'text': 'Victor Robles', 'score': 10},
+        {'text': 'Max', 'score': 15},
+      ]
     },
   ];
 
   int _questionIndex = 0;
+  var _totalScore = 0;
+
+  void _resetQuiz() {
+    setState(() {
+      _questionIndex = 0;
+      _totalScore = 0;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -40,12 +63,17 @@ class _MyAppState extends State<MyApp> {
                 questionIndex: _questionIndex,
                 questions: _questions,
               )
-            : Result(),
+            : Result(
+                resultScore: _totalScore,
+                resetQuiz: _resetQuiz,
+              ),
       ),
     );
   }
 
-  void _answerQuestion() {
+  void _answerQuestion(int score) {
+    _totalScore = _totalScore + score;
+
     if (_questionIndex < _questions.length) {
       print('We have more questions!');
     } else {
--- lib/result.dart
+++ lib/result.dart
@@ -1,12 +1,54 @@
 import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
-  const Result({Key key}) : super(key: key);
+  final int resultScore;
+  final Function resetQuiz;
+
+  const Result({
+    Key key,
+    @required this.resultScore,
+    @required this.resetQuiz,
+  })  : assert(resultScore != null),
+        assert(resetQuiz != null),
+        super(key: key);
+
+  String get resultPhrase {
+    String resultText = "You did it!";
+
+    if (resultScore <= 30) {
+      resultText = "You are awesome and innocent";
+    } else if (resultScore <= 35) {
+      resultText = "Pretty likeable!";
+    } else if (resultScore <= 35) {
+      resultText = "you are... strange";
+    } else {
+      resultText = "you are so bad";
+    }
+
+    return resultText;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text("You did it"),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            resultPhrase,
+            style: TextStyle(
+              fontSize: 36,
+              fontWeight: FontWeight.bold,
+            ),
+            textAlign: TextAlign.center,
+          ),
+          FlatButton(
+            textColor: Colors.blue,
+            child: Text("Restart Quiz"),
+            onPressed: resetQuiz,
+          ),
+        ],
+      ),
     );
   }
 }
